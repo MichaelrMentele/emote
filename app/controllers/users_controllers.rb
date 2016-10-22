3c4721a4cb8bbbf9c99f726fb525ac12fb63a0a5
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-22 09:17:05 -0700

    basic actions

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
+  
   def new
-
+    @user = User.new
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome, #{@user.name}!"
+    else
+      flash[:danger] = "I'm sorry please review your information."
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
