c3d17ed6c04704dd7f57e6944f95b61d743725f1
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-22 11:30:14 -0700

    db setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,70 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018225043) do
+ActiveRecord::Schema.define(version: 20161022161738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "tag",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "invitations", force: :cascade do |t|
-    t.string   "recipient_name",  limit: 255
-    t.string   "recipient_email", limit: 255
-    t.text     "message"
-    t.integer  "inviter_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "token",           limit: 255
-  end
-
-  create_table "queue_items", force: :cascade do |t|
-    t.integer  "position"
-    t.integer  "user_id"
-    t.integer  "video_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "leader_id"
-    t.integer  "follower_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer  "rating"
-    t.integer  "user_id"
-    t.integer  "video_id"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string   "username",        limit: 255
-    t.string   "email",           limit: 255
+    t.string   "email"
+    t.string   "username"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest", limit: 255
-    t.string   "token",           limit: 255
-    t.boolean  "admin"
-  end
-
-  create_table "videos", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description"
-    t.integer  "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "large_cover", limit: 255
-    t.string   "small_cover", limit: 255
-    t.string   "video_url",   limit: 255
   end
 
 end
