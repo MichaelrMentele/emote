a4fc56b6cb68f6f891f43aba7079a7ba5e3b1671
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-29 13:15:58 -0700

    commented out for later

--- spec/features/user_creates_and_uses_random_dispenser_spec.rb
+++ spec/features/user_creates_and_uses_random_dispenser_spec.rb
@@ -1,29 +1,29 @@
-feature "user can create a random messages dispenser" do 
-  scenario "creates and populates random dispenser" do 
-    alice = Fabricate(:user)
-    recipient = Fabricate(:significant, user: alice)
-    m1 = Fabricate(:message, user: alice)
-    m2 = Fabricate(:message, user: alice)
+# feature "user can create a random messages dispenser" do 
+#   scenario "creates and populates random dispenser" do 
+#     alice = Fabricate(:user)
+#     recipient = Fabricate(:significant, user: alice)
+#     m1 = Fabricate(:message, user: alice)
+#     m2 = Fabricate(:message, user: alice)
     
-    login(alice)
+#     login(alice)
 
-    click_link "Create Dispenser" 
-    expect(page).to have_content "Select Your Dispenser"
+#     click_link "Create Dispenser" 
+#     expect(page).to have_content "Select Your Dispenser"
 
-    select dispenser type
-    expect(page).to have_content "Select Your Recipient"
+#     select dispenser type
+#     expect(page).to have_content "Select Your Recipient"
 
-    select recipient 
-    expect(page).to have_content "Enter your configuration details and add messages."  
+#     select recipient 
+#     expect(page).to have_content "Enter your configuration details and add messages."  
 
-    select 0900 from lower bound
-    select 1500 from upper bound
+#     select 0900 from lower bound
+#     select 1500 from upper bound
 
-    select 2x from frequency per day
-    populate dispenser
-    click_button "Create and Begin Sending"
+#     select 2x from frequency per day
+#     populate dispenser
+#     click_button "Create and Begin Sending"
 
-    expect(page).to have_content "Random Sprinkles Dispenser created!"
-    expect(page).to have_content "Welcome to Orchestration Station"
-  end
-end
+#     expect(page).to have_content "Random Sprinkles Dispenser created!"
+#     expect(page).to have_content "Welcome to Orchestration Station"
+#   end
+# end
